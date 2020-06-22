b0a57c6305f86155ff59173f0adf61d8faf4c279
Taslima <taslimapatel26@gmail.com>
2020-06-22 21:21:23 +0100

    level 200 done

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //You can edit ALL of the code here
+let titleHeader = document.querySelector("h4")
+let imageHolder = document.querySelector(".photo")
+let subtitleHolder = document.querySelector('.episode-code');
+let descriptionHolder = document.querySelector('.item-text');
+let sectionCenter = document.querySelector('.section-center');
+let searchBar = document.querySelector("#searchBar");
+
+
+
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  displayMovies(allEpisodes);
   return allEpisodes;
 
 }
@@ -19,38 +29,51 @@ function makePageForEpisodes(episodeList) {
 }
 
 
-function displayMovies() {
   const allEpisodesDetails = setup();
-
-  let displayMovie = allEpisodesDetails.map(function (item) {
-
-
-    return `<article class="movie-item">
-             <img src=${item.image.medium} alt=${item.name} class="photo" />
-           <div class="item-info">
-               <header>
-                 <h4>${item.name}</h4>
-
-               <h4 class="episode-code">${'S0'+item.season.toString()+'E0'+item.number.toString()}</h4>
-               </header>
-             <p class="item-text">
-                ${item.summary}
-             </p>
-            </div>
-          </article>`;
+  searchBar.addEventListener('keyup', (e) => {
+    
+    const searchString = e.target.value.toLowerCase();
+    const filteredMovies = allEpisodesDetails.filter((item) =>{
+      return (
+        item.name.toLowerCase().includes(searchString) ||
+        item.summary.toLowerCase().includes(searchString)
+      );
+    });
+     
+    displayMovies(filteredMovies);
   });
-
-  displayMenu = displayMovie.join(" ");
   
-  sectionCenter.innerHTML = displayMenu;
-}
 
-//window.onload = setup;
-window.onload = displayMovies;
 
-//select all the elements here
-let titleHeader = document.querySelector("h4")
-let imageHolder = document.querySelector(".photo")
-let subtitleHolder = document.querySelector('.episode-code');
-let descriptionHolder = document.querySelector('.item-text');
-let sectionCenter = document.querySelector('.section-center');
+
+function displayMovies(Movies) {
+
+
+   let displayMovie = Movies.map((item)=> {
+
+
+      return `<article class="movie-item">
+              <img src=${item.image.medium} alt=${item.name} class="photo" />
+            <div class="item-info">
+                  <header>
+                    <h4>${item.name}</h4>
+
+                  <h4 class="episode-code">${'S0'+item.season.toString()+'E0'+item.number.toString()}</h4>
+                  </header>
+                <p class="item-text">
+                   ${item.summary}
+                </p>
+               </div>
+             </article>`;
+     });
+
+     displayMenu = displayMovie.join(" ");
+  
+     sectionCenter.innerHTML = displayMenu;
+   }
+
+  window.onload = setup;
+  window.onload = displayMovies;
+
+
+displayMovies();
