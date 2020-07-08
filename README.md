8705ac55291ea571e99d8fb4e8000bf7447f3ad5
Taslima <61126921+Taslima26@users.noreply.github.com>
2020-07-08 09:13:43 +0100

    select episodes and search episodes  not working

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ A starting point for CYF's TV show DOM project
 The requirements for the project are here:
 
 https://github.com/CodeYourFuture/syllabus/tree/master/js-core-3/tv-show-dom-project/readme.md
+
+ <button class='show-button' id='${item.id}'>Click to see all Episodes:</button>
+ <button class='show-name''show-button' id='${item.id}'><h4>${item.name}</h4></button>
--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 let titleHeader = document.querySelector("h4")
 let imageHolder = document.querySelector(".photo")
 let subtitleHolder = document.querySelector('.episode-code');
@@ -7,51 +6,40 @@ let sectionCenter = document.querySelector('.section-center');
 let searchBar = document.querySelector("#searchBar");
 let selectEpisodes = document.querySelector("#select-episode");
 let goBackToAllEpisodes = document.querySelector(".go-back-to-all-episodes");
+let getBackToAllShows=document.querySelector('.go-back-to-all-shows');
 let selectShows = document.querySelector("#select-shows")
+searchBar1 = document.querySelector('#searchBar-1')
 
 
-let episodesData = [];
-let showsData = [];
+let allShows = [];
+let allEpisodes=[]
 
+//fetch all the shows from the api and pass it to display show 
+//function which will dispaly it on the screen/
+const loadShows =  async () => {
+  try {
+    const res=await fetch(`https://api.tvmaze.com/shows`)
+    allShows = await res.json();
+    displayShows(allShows);
+    
+  }
+  catch (err) {
+    console.error(err)
+  }
+};
 
-//1 populate selectShows input with shows ..
-//all episodes must be sorted in alphabetical order
+///Now display all the shows when browser load on the dom..
 
+window.onload = loadShows;
 
-
-
-
-
-function populateShows() {
-  selectShows.options.length = 0;
-
-
-  getAllShows().sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).forEach((item) => {
-
-    let opt = item.name;;
-
-    let el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    selectShows.appendChild(el);
-
-  });
-}
-populateShows();
-///////When my app start I need to display basic informartion about shhow.
-//make a function to display shows and call it when document load.
-//For each show, you must display at least name, image, summary,
-  //genres, status, rating, and runtime.
-
-window.onload=function displayShows() {
-
-
-  let displayEpisode = getAllShows().map((item) => {
+function displayShows(Shows) {
+  
+  let displayShow = Shows.map((item) => {
     return `<article class='show-item'>
            <header>
-            <div class='show-name' id='${item.id}'><h4>${item.name}</h4></div>
+           <h4> <button class='show-button' id='${item.id}'>${item.name}</button></h4>
             </header>
-            <div class='show-info flex-box'>
+            <div>
             <img src=${item.image ? item.image.medium : 'empytyImage'} alt=${item.name} class='show-image flex-box'/>
             <p class='item-text'> ${item.summary}
             </p>
@@ -66,137 +54,87 @@ window.onload=function displayShows() {
             </p>
            </div>
             </div>
-            <button class='show-button' id='${item.id}'>Click to see all Episodes:</button>
+            
 
 
             </article>`;
 
   });
-  displayEpisode = displayEpisode.join('');
-  sectionCenter.innerHTML = displayEpisode;
-  getEpisodesForClickedShow()
+  displayShow = displayShow.join('');
+  sectionCenter.innerHTML = displayShow;
+  getEpisodesForClickedShow();
+
+
 };
 
-//console.log(document.querySelectorAll('.show-button'));
+
+//search shows functionality 
+
+searchBar1.addEventListener('keyup', (e) => {
+
+  const searchString = e.target.value.toLowerCase();
+ 
+  
+  const filteredShows = allShows.filter((item) => {
+    return (
+      item.name.toLowerCase().includes(searchString) ||
+      item.summary.toLowerCase().includes(searchString)
+    );
+  });
+  displayShows(filteredShows);
+});
+ 
+
+//get back to allShows
+
+getBackToAllShows.addEventListener('click', () => {
+  displayShows(allShows);
+})
+
+//when user click on a show name it should get all the episodes for that particular shows.
+
 function getEpisodesForClickedShow() {
-  let allButton = document.querySelectorAll(".show-button");
-  allButton.forEach((button) => {
+  let allShowsButton = document.querySelectorAll('.show-button');
+ 
+ 
+  allShowsButton.forEach((button) => {
     button.addEventListener("click", (e) => {
-      console.log("I am clicked");
-      console.log(e.target.id);
-      selectShowsToGetEpisodes(e.target.id);
       
+      getEpisodesAcordingToShow(e.target.id);
+      
+
     });
    
+
   });
- 
+
 }
-
- 
-
-
-
+//Now I have access to particular show id I am going to go 
+//and fetch api of episodes of that show.
+async function getEpisodesAcordingToShow(id){
 
 
-
-
-
-///Now I have all the shows so need to  get access to  one show which user selected in select box.
-//sort the shows
-document.querySelector("#select-shows").addEventListener('change', (e) => {
-  const selectString = e.target.value;
-  console.log(selectString);
-  let selectedShow = getAllShows().filter((item) => {
-
-    return (
-      item.name === selectString);
-  });
-  let idOfShow = selectedShow.map(data => data.id);
-  fetch(`https://api.tvmaze.com/shows/${idOfShow}/episodes`)
-    .then((response) => response.json())
-    .then((episodesOfShow) => {
-      displayEpisodes(episodesOfShow);
-      searchEpisodes(episodesOfShow);
-      populateEpisodes(episodesOfShow);
-      selectEpisodesToDisplay(episodesOfShow);
-      goBackToAllEpisodesFunction(episodesOfShow);
-
-
-    });
-});
-function selectShowsToGetEpisodes(id) {
-  
-  
   //Now I have an access to a particular shows that user serlect from the select box so I need
   //to go and fetch episodes of that selected show.
 
   //fetch an api based on selcted show
-  fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
-    .then((response) => response.json())
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}/episodes`)
+  allEpisodes = await res.json()
     .then((episodesOfShow) => {
       displayEpisodes(episodesOfShow);
-      searchEpisodes(episodesOfShow);
       populateEpisodes(episodesOfShow);
-      selectEpisodesToDisplay(episodesOfShow);
-      goBackToAllEpisodesFunction(episodesOfShow);
-
-
+      searchEpisodes(episodesOfShow);
+      
+     
     });
-  
 }
 
-
-
-
-selectShowsToGetEpisodes();
-//implement the search functionality for the selected shows episodes
-function searchEpisodes(episodes) {
-  if (episodes !== null) {
-    let totalEpisodes = episodes.length;
-    let total = document.querySelector(".total-1")
-    total.textContent = totalEpisodes;
-  }
-  searchBar.addEventListener('keyup', (e) => {
-
-    const searchString = e.target.value.toLowerCase();
-    const filteredMovies = episodes.filter((item) => {
-      return (
-        item.name.toLowerCase().includes(searchString) ||
-        item.summary.toLowerCase().includes(searchString)
-      );
-    });
-    displayEpisodes(filteredMovies);
-  });
-}
-
-//populate selectbox with corrosponding epsodes
-
-function populateEpisodes(allEpisodes) {
-  selectEpisodes.length = 0;
-allEpisodes.forEach((item) => {
-
-    let opt = `S${item.season.toString().padStart(2,0)}E${item.number.toString().padStart(2,0)}-${item.name}`;
-
-
-
-  let element = document.createElement('option')
-    element.textContent = opt;
-    element.value = opt;
-    selectEpisodes.add(element);
-
-  });
-
-
-}
-
-//Now I have all the episodes of corrosponding show ,I need to find a
-//way to display them.
-function displayEpisodes(allShows) {
-  let EpisodeBeingDisplay = allShows.length;
+function displayEpisodes(episodes) {
+  let EpisodeBeingDisplay = episodes.length;
   let total = document.querySelector(".total-2")
   total.textContent = EpisodeBeingDisplay;
 
-  let displayEpisode = allShows.map((item) => {
+  let displayEpisode = episodes.map((item) => {
     return `<article class='movie-item'>
             <img src=${item.image ? item.image.medium : 'empytyImage'} alt=${item.name} class='photo'/>
             <div class='item-info'>
@@ -215,15 +153,72 @@ function displayEpisodes(allShows) {
 }
 
 
-///Now implement selectBox funionality episode selection;
 
-function selectEpisodesToDisplay(episodesData) {
+///populate select show drop down list
+//// They said in instruction I can use  getAllShows function here.
+
+function populateShows() {
+  selectShows.options.length = 0;
+
+
+  getAllShows().sort((a, b) => (a.name > b.name) ? 1 : ((b.name > a.name) ? -1 : 0)).forEach((item) => {
+
+    let opt = item.name;;
+
+    let el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    selectShows.appendChild(el);
+
+  });
+}
+
+populateShows();
+
+//when user click on a particular show I should go and get all episodes for that show.
+
+document.querySelector("#select-shows").addEventListener('change', (e) => {
+  const selectString = e.target.value;
+  console.log(selectString);
+  let selectedShow = getAllShows().filter((item) => {
+
+    return (
+      item.name === selectString);
+  });
+  let idOfShow = selectedShow.map(data => data.id);
+  //This will go and get all the episodes for the chosen show;
+  console.log('result when i calll function with id',getEpisodesAcordingToShow(idOfShow));
+ // populateEpisodes(episodesForSelectBox);
+});
+
+
+//This will populate select box with all the episodes of chosen shows.
+function populateEpisodes(allEpisodes) {
+  selectEpisodes.length = 0;
+ // console.log('all Episodes',allEpisodes);
+  allEpisodes.forEach((item) => {
+
+ let opt = `S${item.season.toString().padStart(2, 0)}E${item.number.toString().padStart(2, 0)}-${item.name}`;
+
+
+
+    let element = document.createElement('option')
+    element.textContent = opt;
+    element.value = opt;
+    selectEpisodes.add(element);
+
+  });
+
+
+}
+
+
   document.querySelector("#select-episode").addEventListener('change', (e) => {
     const selectString = e.target.value;
     console.log(selectString);
-    const selectedEpisodes = episodesData.filter((item) => {
+    const selectedEpisodes = allEpisodes.filter((item) => {
       return (
-      `S${item.season.toString().padStart(2,0)}E${item.number.toString().padStart(2,0)}-${item.name}` === selectString
+        `S${item.season.toString().padStart(2, 0)}E${item.number.toString().padStart(2, 0)}-${item.name}` === selectString
 
       );
     });
@@ -233,44 +228,25 @@ function selectEpisodesToDisplay(episodesData) {
 
   });
 
+
+
+//search episodes functionality
+
+
+function searchEpisodes(allEpisodes) {
+  searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value;
+    console.log(searchString);
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      return (episode.name.toLowerCase().includes = searchString ||
+        episode.summary.toLowerCase().includes(searchString));
+  
+    })
+    displayEpisodes(filteredEpisodes);
+    
+  });
 }
-
-///Implement go back to all episodes functionality.
-function goBackToAllEpisodesFunction(everything){
-  goBackToAllEpisodes.addEventListener('click', () => {
-    displayEpisodes(everything);
-  })
-}
-
-
-//display episoded of the shows when user click on particular show.
-
-
-
-//footer functionality to give credit to fetch api
-function makePageForEpisodes() {
-const rootElem = document.getElementById("root");
-
-
-  var a = document.createElement('a');
-  var linkText = document.createTextNode("This data is coming from tvmaze.com,Please click here for more info .Copyrights reserve at @ https://www.tvmaze.com/");
-  a.appendChild(linkText);
-  a.title = "The data is comin from tvmaze.com/api#licensing";
-  a.href = "https://www.tvmaze.com/";
-  rootElem.appendChild(a);
-
-}
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
